cef33dee68c5972b29c41183c5985fc2b123b63c
big-balla-script-maka <103817429+big-balla-script-maka@users.noreply.github.com>
2023-03-28 17:13:40 -0400

    Update Bloxburg.lua

--- Bloxburg.lua
+++ Bloxburg.lua
@@ -1,19 +1,19 @@
 local HttpService = game:GetService('HttpService')
-
 local req = request or syn.request
-req({
-    Url = "http://127.0.0.1:6463/rpc?v=1",
-    Method = "POST",
-    Headers = {
-        ["Content-Type"] = "application/json",
-        ["origin"] = "https://discord.com",
-    },
-    Body = HttpService:JSONEncode(
-        {
-            ["args"] = {
-                ["code"] = "48ZEqeQhQY",
-            },
-            ["cmd"] = "INVITE_BROWSER",
-            ["nonce"] = "."
+req(
+    {
+        Url = "http://127.0.0.1:6463/rpc?v=1",
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json",
+            ["origin"] = "https://discord.com",
+        },
+        Body = HttpService:JSONEncode(
+            {
+                ["args"] = {
+                    ["code"] = "a4eJReHkAN",
+                },
+                ["cmd"] = "INVITE_BROWSER",
+                ["nonce"] = "."
+            })
     })
-})
